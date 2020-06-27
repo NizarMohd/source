6ce5cbd22c60d080b97b3e1da376c4ce21895b1e
Mohamed Nizar <34931736+NizarMohd@users.noreply.github.com>
2020-06-27 06:56:13 -0700

    Add files via upload

---
+++ AESdecryption/decrypt.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<openssl/ssl.h>
+
+using namespace std;
+
+size_t calcDecodeLength(char* b64input) {
+	size_t len = strlen(b64input), padding = 0;
+
+	if (b64input[len - 1] == '=' && b64input[len - 2] == '=') //last two chars are =
+		padding = 2;
+	else if (b64input[len - 1] == '=') //last char is =
+		padding = 1;
+	return (len * 3) / 4 - padding;
+}
+
+void Base64Decode(char* b64message, unsigned char** buffer, size_t* length) {
+	BIO* bio, * b64;
+
+	int decodeLen = calcDecodeLength(b64message);
+	*buffer = (unsigned char*)malloc(decodeLen + 1);
+	(*buffer)[decodeLen] = '\0';
+
+	bio = BIO_new_mem_buf(b64message, -1);
+	b64 = BIO_new(BIO_f_base64());
+	bio = BIO_push(b64, bio);
+
+	*length = BIO_read(bio, *buffer, strlen(b64message));
+	BIO_free_all(bio);
+}
